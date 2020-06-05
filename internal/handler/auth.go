d46574d6335f16fa5f5514d921583462ad939668
Artyom Barhanov <darakon92@gmail.com>
2020-06-05 14:39:36 +0300

    Implement Auth handler

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -3,10 +3,19 @@ package handler
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	srvUsers "github.com/rtemb/srv-users/pkg/client/srv-users"
 )
 
 func (h *Handler) Auth(ctx context.Context, req *srvUsers.AuthRequest) (*srvUsers.AuthResponse, error) {
 	r := &srvUsers.AuthResponse{}
+
+	token, err := h.service.Auth(req.Email, req.Password)
+	if err != nil {
+		return r, errors.Wrap(err, "unable to authenticate")
+	}
+
+	r.Token = token
+	r.Valid = true
 	return r, nil
 }
