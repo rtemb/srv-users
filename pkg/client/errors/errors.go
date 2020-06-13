ab911ae1a25eccb0cc9692458b989b6d2583ce30
Artyom Barhanov <artem.barkhanov@karhoo.com>
2020-06-13 14:19:17 +0300

    Added client error

--- pkg/client/errors/errors.go
+++ pkg/client/errors/errors.go
@@ -6,4 +6,5 @@ import "errors"
 var (
 	UnableToCreateUser = errors.New("unable to create user")
 	UserAlreadyExists  = errors.New("user already exist")
+	UnableToAddRole    = errors.New("unable to add role")
 )
