e19a35870a723f07b449937eca171a9b24df5b58
Artyom Barhanov <artem.barkhanov@karhoo.com>
2020-06-13 23:24:57 +0300

    Add UserNotFound error

--- pkg/client/errors/errors.go
+++ pkg/client/errors/errors.go
@@ -1,10 +1,14 @@
 package errors
 
-import "errors"
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 // nolint
 var (
-	UnableToCreateUser = errors.New("unable to create user")
-	UserAlreadyExists  = errors.New("user already exist")
-	UnableToAddRole    = errors.New("unable to add role")
+	UnableToCreateUser = status.Error(codes.Internal, "unable to create user")
+	UserAlreadyExists  = status.Error(codes.FailedPrecondition, "user already exist")
+	UnableToAddRole    = status.Error(codes.Internal, "unable to add role")
+	UserNotFound       = status.Error(codes.NotFound, "unable to find user")
 )
